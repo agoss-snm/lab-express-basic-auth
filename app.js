df32b52ed54662a163609fe4c63ad6de483f04fc
Agostina Romero <agostinaromontamatt@gmail.com>
2023-04-22 14:38:27 -0300

    solvedFirstItineration

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
 require('dotenv/config');
-
-// ℹ️ Connects to the database
 require('./db');
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require('express');
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
-
 const app = express();
-
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
@@ -28,8 +16,14 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
 module.exports = app;
+
 
---
+++ routes/auth.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const User = require("../models/User.model");
+
+router.get("/signup", (req, res) => {
+    res.render("auth/signup");
+  });
+
+  router.post('/signup', async (request, response) => {
+    const { email, password } = request.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      await User.create({
+        email: email,
+        password: hashedPassword
+      });
+      console.log(email, hashedPassword);
+      response.redirect('/signup');
+    } catch (error) {
+      console.log(error);
+      response.render('auth/signup', { error: 'An error occurred' });
+    }
+  });
+  
+  
+  module.exports = router;
+
+  
