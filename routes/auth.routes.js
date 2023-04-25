30e3b87fb08f79f43fd749d5f7358549d7b9c980
Agostina Romero <agostinaromontamatt@gmail.com>
2023-04-25 05:43:51 -0300

    secondItinerationDone

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -84,7 +84,14 @@ router.post('/login', async(req, res, next) => {
     .catch(error => next(error));
 });
 
-// userProfile route and the module export stay unchanged
+// Router Post Logout
+
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
 
 
 
